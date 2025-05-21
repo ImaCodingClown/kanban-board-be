4115b969a3a74b60ef0740fd6f540a991be2cd22
David Oh <davoh07290@gmail.com>
2025-05-21 18:34:45 -0400

    add username to the User model

--- src/services/auth.rs
+++ src/services/auth.rs
@@ -4,6 +4,7 @@ use bcrypt::{hash, verify};
 use mongodb::{bson::doc, Client, Collection};
 
 pub async fn signup(
+    username: String,
     email: String,
     password: String,
     db: &Client,
@@ -22,8 +23,18 @@ pub async fn signup(
         return Err("Email already in use".into());
     }
 
+    if users
+        .find_one(doc! { "username": &username })
+        .await
+        .unwrap()
+        .is_some()
+    {
+        return Err("Username already in use".into());
+    }
+
     let hashed = hash(&password, 4).unwrap();
     let user = User {
+        username: username.clone(),
         id: None,
         email: email.clone(),
         password_hash: hashed,
@@ -34,6 +45,7 @@ pub async fn signup(
 }
 
 pub async fn login(
+    username: String,
     email: String,
     password: String,
     db: &Client,
@@ -41,9 +53,14 @@ pub async fn login(
 ) -> Result<String, String> {
     let users: Collection<User> = db.database("general").collection("users");
 
-    if let Some(user) = users.find_one(doc! { "email": &email }).await.unwrap() {
-        if verify(password, &user.password_hash).unwrap() {
-            return Ok(create_jwt(&email, secret));
+    let username_opt = users
+        .find_one(doc! { "username": &username })
+        .await
+        .unwrap();
+
+    if let Some(user) = username_opt {
+        if user.email == email && verify(password, &user.password_hash).unwrap() {
+            return Ok(create_jwt(&user.email, secret));
         }
     }
 
