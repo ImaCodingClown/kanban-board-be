49feca74eab0f1ebecb86bee37768e28320dd0ba
Tony <tonypierrekim@gmail.com>
2025-06-01 02:25:59 -0400

    WIP

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -1,15 +1,21 @@
 use crate::models::auth::Claims;
 use jsonwebtoken::{encode, EncodingKey, Header};
 
-pub fn create_jwt(user_id: &str, secret: &str) -> String {
-    let claims = Claims {
-        sub: user_id.to_owned(),
-        exp: 200000,
-    };
-    encode(
-        &Header::default(),
-        &claims,
-        &EncodingKey::from_secret(secret.as_bytes()),
-    )
-    .unwrap()
+pub struct JWTValidator {}
+
+pub trait JWTMethods {
+    fn create_jwt(user_id: &str, secret: &str) -> String {
+        let claims = Claims {
+            sub: user_id.to_owned(),
+            exp: 200000,
+        };
+        encode(
+            &Header::default(),
+            &claims,
+            &EncodingKey::from_secret(secret.as_bytes()),
+        )
+        .unwrap()
+    }
 }
+
+impl JWTMethods for JWTValidator {}
