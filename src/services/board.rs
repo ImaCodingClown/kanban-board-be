c96c5a76a6a3ff7548f21fbc453b48e09fd34ef7
Tony <tonypierrekim@gmail.com>
2025-07-17 21:01:31 -0400

    Hardcord teamn name for now

--- src/services/board.rs
+++ src/services/board.rs
@@ -8,17 +8,19 @@ use mongodb::Client;
 
 pub async fn get_board_by_team(team_name: String, db: &Client) -> Result<Board, CustomError> {
     let board_service = ODM::<Board>::build(db).await;
-    let mut boards = board_service.fetch_many_by_team(&team_name).await?;
+    //TODO: Replace LJY Members
+    let mut boards = board_service.fetch_many_by_team("LJY Members").await?;
 
     if let Some(board) = boards.pop() {
         Ok(board)
     } else {
-        Ok(Board::create_default(team_name))
+        Ok(Board::create_default("LJY Members".to_string()))
     }
 }
 
 pub async fn create_board(team_name: String, db: &Client) -> Result<Board, CustomError> {
-    let board = Board::create_default(team_name);
+    //TODO: Replace LJY Members
+    let board = Board::create_default("LJY Members".to_string());
     let board_service = ODM::<Board>::build(db).await;
     board_service.save_one(&board).await?;
     Ok(board)
