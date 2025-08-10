b5e27f51c092c3545d8d2ed65113bb5f27ffb0a3
Tony <tonypierrekim@gmail.com>
2025-08-10 10:18:35 -0400

    Hotfix: Duplicate codes

--- src/models/cards.rs
+++ src/models/cards.rs
@@ -94,22 +94,6 @@ pub struct EditCardPayload {
     pub team: String,
 }
 
-#[derive(Deserialize, Debug)]
-pub struct DeleteCardPayload {
-    pub card_id: String,
-    pub column_name: String,
-    pub team: String,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct EditCardPayload {
-    pub card_id: String,
-    pub title: String,
-    pub description: String,
-    pub column_name: String,
-    pub team: String,
-}
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Column {
     pub title: String,
